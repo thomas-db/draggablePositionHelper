a4175bbb4fa535ea91824151cd5b769a2501402a
Debrabelge <Thomas-db@hotmail.fr>
2020-02-10 00:49:19 +0100

    added draggablePositionHelper class

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
+class draggablePositionHelper {
+    /**
+     * Init draggable element and its parent area
+     * @param {String} parentElementId element where draggable element will be moving
+     * @param {String} draggableElementId element that will be draggable
+     */
+    constructor(parentElementId, draggableElementId) {
+        // this.parentElement = document.getElementById(parentElementId);
+        // this.draggableElement = document.getElementById(draggableElementId);
+
+        console.log("npm update");
+    }
+
+    testConsole(param) {
+        console.log(param);
+    }
+}
+
 const magicString = () => {
-    return 'Hello World';
+    return 'Hello World wheywhey';
 };
 
 const displayMyName = (name) => {
     return `Your name is ${name}`;
 };
 
-module.exports = {
+export default {
     magicString,
-    displayMyName
+    displayMyName,
+    draggablePositionHelper
 };
